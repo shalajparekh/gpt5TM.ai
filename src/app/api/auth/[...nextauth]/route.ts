a07b0c5b8fac462b1a813570558352219e246594
Shalaj Parekh <shalaj.parekh@gmail.com>
2025-08-13 17:14:02 +0530

    Initial commit

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -1,7 +1,7 @@
 import NextAuth from "next-auth";
 import { authConfig } from "@/auth";
 
-const handler = NextAuth(authConfig);
+const handler = NextAuth(authConfig as any);
 export { handler as GET, handler as POST };
 export const runtime = "nodejs";
 export const dynamic = "force-dynamic";
